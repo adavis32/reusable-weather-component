e90206d397b9523b0f8a901d3fbe842650b9e4ec
Austin Davis <adavisdesigns25@gmail.com>
2016-12-26 19:25:05 -0500

    angular2-skeleton
    no node_modules

---
+++ app/main.js
@@ -0,0 +1,8 @@
+"use strict";
+//modules
+var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+//components
+var app_module_1 = require("./app.module");
+var platform = platform_browser_dynamic_1.platformBrowserDynamic();
+platform.bootstrapModule(app_module_1.AppModule);
+//# sourceMappingURL=main.js.map
---
+++ app/main.ts
@@ -0,0 +1,8 @@
+//modules
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+
+//components
+import {AppModule} from './app.module';
+
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
